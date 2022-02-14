a6dbfb176da579f55ab477e24b2c9a803ba466c6
nadiaf85 <nadia.fiorio@alice.it>
2022-02-14 17:28:52 +0100

    push

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
-$paragrafo = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores quam cum quia consectetur dolorum reiciendis hic natus autem! Pariatur dolore, veritatis ducimus beatae doloribus incidunt officiis magnam maxime consectetur quam."
+$paragrafo = "Di origine preistorica, l'abitato venne rifondato dai Romani all'interno dell'ansa del fiume intorno alla metà
+    del I secolo a.C., rimanendo sotto il governo dell'Impero fino al V secolo, quando venne occupato dal re germanico Teodorico il Grande.
+    Entrò a far parte del dominio dei Longobardi prima e dei Franchi poi, rimanendo fedele nei secoli successivi agli imperatori del Sacro
+    Romano Impero. Divenne libero Comune all'inizio del XII secolo per poi prosperare sotto la Signoria degli Scaligeri. Si dedicò alla
+    Serenissima nel 1405, passando sotto il governo della Repubblica di Venezia. Occupata militarmente da Napoleone nel 1797, nel 1815
+    divenne parte dell'Impero austriaco che la trasformò nella sua maggiore piazzaforte militare in territorio italico, per essere annessa
+    al Regno d'Italia nel 1866. Verona è stata dichiarata patrimonio dell'umanità dall'UNESCO per le peculiarità urbanistiche e per il patrimonio
+    artistico e culturale. Il suo simbolo è l'Arena ed è conosciuta nel mondo per l'opera di William Shakespeare Romeo e Giulietta.
+    Sede universitaria e importante snodo di scambio logistico e intermodale tramite il Quadrante Europa, l'industria riveste un ruolo
+    chiave nell'economia della città, come il turismo fieristico e culturale.";
+
+$lunghezzaParagrafo = strlen($paragrafo);
+$parolaDaCensurare = $_GET["militarmente"];
+
+$paragrafoCensurato = str_replace($parolaDaCensurare, "***", $paragrafo);
+$lunghezzaParagrafoCensurato = strlen($paragrafoCensurato);
 
 ?>
 
@@ -15,6 +30,10 @@ $paragrafo = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperior
 <body>
 
 <p><?php echo $paragrafo; ?></p>
+<p><?php echo $lunghezzaParagrafo; ?></p>
+
+<p><?php echo $paragrafoCensurato; ?></p>
+<p><?php echo $lunghezzaParagrafoCensurato; ?></p>
 
 </body>
 </html>
