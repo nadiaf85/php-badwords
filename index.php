df1f7909c7efdd66a0fad79c4d92a0e74b31a2b9
nadiaf85 <nadia.fiorio@alice.it>
2022-02-14 15:40:31 +0100

    creata repo

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$paragrafo = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores quam cum quia consectetur dolorum reiciendis hic natus autem! Pariatur dolore, veritatis ducimus beatae doloribus incidunt officiis magnam maxime consectetur quam."
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<p><?php echo $paragrafo; ?></p>
+
+</body>
+</html>
+
+
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall’utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) 
+tutte le occorrenze della parola da censurare. -->
